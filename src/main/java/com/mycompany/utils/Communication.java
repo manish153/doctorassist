4e97859567552af41657e14cd7c89db6f286fac7
manish <manish.rao@outlook.com>
2014-01-20 22:13:47 -0800

    interface for email communication

---
+++ src/main/java/com/mycompany/utils/Communication.java
@@ -0,0 +1,10 @@
+package com.mycompany.utils;
+
+public interface Communication {
+
+	  public void SendEmail(String To);
+	  public void SendEmail(String To, String Cc);
+	  public void SendEmail(String To, String Cc, String Bcc);
+      
+
+}
